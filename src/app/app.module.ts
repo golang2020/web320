dc4bf1466b6a704c5bce16788733147b9fbd101c
Gang Liu <68601266+golang2020@users.noreply.github.com>
2020-07-21 20:26:29 -0500

    Add style for menubar
    Add style for menubar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SpinningCubeComponent } from '../examples/spinning-cube/spinning-cube.component';
 import { View3dComponent } from './components/view3d/view3d.component';
 import { Data3dComponent } from './components/data3d/data3d.component';
+import { MenubarComponent } from './menubar/menubar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SpinningCubeComponent,
     View3dComponent,
-    Data3dComponent
+    Data3dComponent,
+    MenubarComponent
   ],
   imports: [
     BrowserModule,
