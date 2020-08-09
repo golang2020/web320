57502bf2b1d77362864f7a734b9797335ea7863e
Gang Liu <68601266+golang2020@users.noreply.github.com>
2020-08-09 10:25:42 -0500

    Install dat.GUI library
    Install dat.GUI library, and use control the light with dat.GUI.

--- src/app/components/data3d/data3d.component.ts
+++ src/app/components/data3d/data3d.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-data3d',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data3d.component.scss']
 })
 export class Data3dComponent implements OnInit {
-
-  constructor() { }
+  static ID : number = 0;
+  //wrap the object3d in THREEjs
+  private object3d: THREE.Object3D  = new THREE.Object3D();
+  // can be found by calling scene.getObjectByName(name)
+  constructor() { 
+    this.object3d.name = 'Data3dComponent'+Data3dComponent.ID;
+    Data3dComponent.ID++;
+  }
 
   ngOnInit(): void {
   }
 
+  getObject3D(): THREE.Object3D {
+    return this.object3d;
+  }
+
+  getName(): string {
+    return this.object3d.name;
+  }
 }
--- src/examples/spinning-cube/spinning-cube.component.ts
+++ src/examples/spinning-cube/spinning-cube.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2, ViewEncapsulation } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef, Renderer2, ViewEncapsulation, ComponentFactoryResolver } from '@angular/core';
+import * as dat from 'dat.gui';
 import * as THREE from 'three';
 import { Vector3 } from 'three';
 import OrbitControls from 'orbit-controls-es6';
-//import { View3dComponent } from '../view3d/view3d.component';
+import { error } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-spinning-cube',
@@ -20,13 +20,13 @@ export class SpinningCubeComponent implements OnInit {
   private scene: THREE.Scene = new THREE.Scene();
   private camera: THREE.PerspectiveCamera;
   private renderer: THREE.WebGLRenderer;
-  //private view3d: View3dComponent;
+  private guiControl: dat.GUI;
   private cube: THREE.Mesh;
   private controls: OrbitControls;
-  private animationDisplay;
   private nearPlane: number = 0.1;
   private farPlane: number = 1000;
-  private fieldView: number = 75;
+  private fieldView: number = 45;
+  private moveZDir: boolean = true;
 
   constructor(private element: ElementRef, private ngRenderer: Renderer2) { 
 
@@ -38,13 +38,57 @@ export class SpinningCubeComponent implements OnInit {
     this.renderer.setSize(this.width, this.height);
     this.camera = new THREE.PerspectiveCamera(this.fieldView, this.width/this.height,
                                               this.nearPlane, this.farPlane);
-    //this.topGroup = new THREE.Group();
     
     this.renderer.setPixelRatio(window.devicePixelRatio);
-    //
+    this.renderer.setClearColor('rgb(255,255,255)');
+    //use Group for animation rig
+    let cameraPosition = new THREE.Group();
+    cameraPosition.name = 'cameraPosition';
+    cameraPosition.add(this.camera);
+    this.scene.add(cameraPosition);
+    //add model
+    this.getModel();
+    //grid
+    this.scene.add( new THREE.GridHelper( 20, 20, 0x0c610c, 0x444444 ) );
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.minDistance = 1;
+    this.controls.mazDistance = 1000;
+    this.camera.position.z = 25;//adjust it based on the bounding box of world
+    this.camera.position.x = 10;
+    this.camera.lookAt(this.scene.position);
+    this.initScene();
+    
+    window.addEventListener('resize', () => {
+          this.onResize();
+      });
+    
+    this.getLight();
+    
+  }
+  
+  getLight() {    
+    this.scene.add( new THREE.AmbientLight( 0x909090 ) );
+    //gui
+    this.guiControl = new dat.GUI();
+    let lightGroup = this.guiControl.addFolder('Light');
+    //control the light
+    let light = new THREE.PointLight('rgb(255,255,255)');
+    light.position.y = 20;
+    light.intensity = 2;
+    this.scene.add(light);
+    lightGroup.add(light, 'intensity', 0, 10);
+    lightGroup.add(light.position, 'y', 10, 50);
+  }
+
+  getModel() {
+    let group = new THREE.Object3D();
+    group.translateY(5);
+    this.scene.add(group);
+    // cube
     this.cube = this.createCube(2.5, new THREE.Color('rgb(255,96,70)'));
     this.cube.position.set(0, 0, -0.8);
-    this.scene.add(this.cube);
+    group.add(this.cube);
     // line
     let lineMaterial = new THREE.LineBasicMaterial( {
       color: 0x00ff00,
@@ -54,11 +98,12 @@ export class SpinningCubeComponent implements OnInit {
     points.push( new THREE.Vector3( -10, 0, 0 ) );
     points.push( new THREE.Vector3( 0, 10, 0 ) );
     points.push( new THREE.Vector3( 10, 0, 0 ) );
-    
+
     let lineGeometry = new THREE.BufferGeometry().setFromPoints( points );
     this.scene.add( new THREE.Line( lineGeometry, lineMaterial ) );
-    
+
     //create a quad geometry
+    
     let geometry = new THREE.Geometry();
     geometry.vertices.push( new THREE.Vector3( -5, -5, 0 ) );
     geometry.vertices.push( new THREE.Vector3(  5, -5, 0 ) );
@@ -81,27 +126,32 @@ export class SpinningCubeComponent implements OnInit {
       polygonOffsetFactor: 1, 
       polygonOffsetUnits: 1
     } );
-    let mesh = new THREE.Mesh( geometry, material );
-    this.scene.add( mesh );
+    let mesh = new THREE.Mesh( geometry, material );    
+    group.add( mesh );
     //outline of mesh
     let geo = new THREE.EdgesGeometry( geometry ); 
     let mat = new THREE.LineBasicMaterial( { color: 0xff0000, linewidth: 1 } );
     mesh.add( new THREE.LineSegments( geo, mat ) );
-    //grid
-    this.scene.add( new THREE.GridHelper( 20, 20, 0x0c610c, 0x444444 ) );
 
-    this.scene.add( new THREE.AmbientLight( 0x909090 ) );
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    this.controls.minDistance = 1;
-    this.controls.mazDistance = 1000;
-    this.camera.position.z = 15;
-    this.camera.lookAt(this.scene.position);
-    this.initScene();
-      window.addEventListener('resize', () => {
-          this.onResize();
-      });
+    //plane
+    let planeSize = 5;
+    let plane = new THREE.PlaneGeometry(planeSize, planeSize);
+    let planeMaterial = new THREE.MeshPhongMaterial({
+      color: 0x0000ff,
+      side: THREE.DoubleSide
+    });
+    let texture = new THREE.TextureLoader().load( '/assets/textures/0.jpg' );
+    if (texture != undefined) {
+      texture.wrapS = THREE.RepeatWrapping;
+      texture.wrapT = THREE.RepeatWrapping;
+      texture.repeat.set(1.0,1.0);
+      planeMaterial.map = texture;
+    }
+    
+    plane.rotateX(Math.PI/2);
+    group.add(new THREE.Mesh( plane, planeMaterial));
   }
-  
+
   initScene() {
 
     let vrButtonOptions = {
@@ -110,8 +160,7 @@ export class SpinningCubeComponent implements OnInit {
         corners: 'square'
     };
     this.ngRenderer.appendChild(this.element.nativeElement, this.renderer.domElement);
-    
-    this.animationDisplay = window;
+
     window.requestAnimationFrame(() => {
       this.update();
     });
@@ -121,9 +170,23 @@ export class SpinningCubeComponent implements OnInit {
 
     this.cube.rotateY(0.03);
     this.cube.rotateZ(0.03);
+
+    let cameraZPos = this.scene.getObjectByName('cameraPosition');
+    if (cameraZPos != null) {
+      // adjust the position of camera
+      if (this.moveZDir) {
+        cameraZPos.position.z -= 0.02;// animation step
+      } else {
+        cameraZPos.position.z += 0.02;
+      }
+      if (cameraZPos.position.z < 0)
+        this.moveZDir = false;
+      if (cameraZPos.position.z >20)//Max. of world in Z direction
+        this.moveZDir = true;
+    }
     //this.view3d.render();
     this.renderer.render(this.scene, this.camera);
-    this.animationDisplay.requestAnimationFrame(() => {
+    window.requestAnimationFrame(() => {
         this.update();
     });
 
